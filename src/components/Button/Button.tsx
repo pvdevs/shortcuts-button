66a4e4105f64fb23d03c6ee5909b16f529f2c119
Paulo Victor <paulovictor.devs@gmail.com>
2024-01-22 17:29:17 -0300

    add border hover animation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,9 @@ import './styles/Button.scss';
 export const Button = () => {
   return (
     <button className="btn">
-      <div className="btn-bg"></div>
-
       <span className="btn-text-out">Discover more of our work</span>
       <span className="btn-text-in">Discover more of our work</span>
+      <div className="btn-bg"></div>
     </button>
   );
 };
