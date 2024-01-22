39cc84125726d35d4b6666208736bbf2c6a5a7b4
Paulo Victor <paulovictor.devs@gmail.com>
2024-01-22 17:04:31 -0300

    add text in and out animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
+import './App.scss';
 import { Button } from './components/Button/Button';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="content-container">
       <Button />
-    </>
+    </div>
   );
 }
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
 import './styles/Button.scss';
 
 export const Button = () => {
-  return <></>;
+  return (
+    <button className="btn">
+      <div className="btn-bg"></div>
+
+      <span className="btn-text-out">Discover more of our work</span>
+      <span className="btn-text-in">Discover more of our work</span>
+    </button>
+  );
 };
